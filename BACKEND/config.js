fa8e62b5037f769679fbbc0f0a7016bc41303f43
Skullkrash <jeannotnaegely@gmail.com>
2025-01-10 10:05:34 +0100

    added BACKEND files from cnamapp - nodeJSAvecBDD

---
+++ BACKEND/config.js
@@ -0,0 +1,10 @@
+module.exports =  {
+    ACCESS_TOKEN_SECRET : "EMMA123",
+    BDD : {
+    "host" :"dpg-cu0dgvdds78s73dc2isg-a.oregon-postgres.render.com",
+    "port" : "5432",
+    "user" : "cnam_83cz_user",
+    "password" : "q8coO1tzZsmqA6b2N5PZ5BeupSKuMRu4",
+    "bdname" :"cnam_83cz" 
+    }
+}
